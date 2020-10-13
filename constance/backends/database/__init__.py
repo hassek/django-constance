345dbb27bfc73bba72cc8df33fdd0911ae40c104
Tomas Henriquez <nullwriter@gmail.com>
2020-10-13 18:34:57 +0200

    removed unused import

--- constance/backends/database/__init__.py
+++ constance/backends/database/__init__.py
@@ -1,5 +1,4 @@
 from django.core.cache import caches
-from django.core.cache.backends.locmem import LocMemCache
 from django.core.exceptions import ImproperlyConfigured
 from django.db import (
     IntegrityError,
